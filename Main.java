22531cb0cac5bbb750b3b51d72ab0ef058c0c8f3
Chanel Belvin <68121865+chanellw@users.noreply.github.com>
2024-08-14 12:25:21 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+
+  public static void main(String[] args){
+//Output a sentence that prints your name
+//Find your favorite quote, format it and print it out
+//Draw a picture using symbols and the escape sequences
+    
+  }
+
+
+  
+}
